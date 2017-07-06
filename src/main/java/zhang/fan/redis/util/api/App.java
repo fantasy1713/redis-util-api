f2b81e504f7bc073fb4d4b9bbd067383f89d82de
phan <phan@fantasy1713@qq.com>
2017-07-07 00:14:07 +0800

    init
    init

---
+++ src/main/java/zhang/fan/redis/util/api/App.java
@@ -0,0 +1,13 @@
+package zhang.fan.redis.util.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
